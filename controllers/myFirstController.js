4e757174a75c2c3786ce1bebe67ea17a2d481d85
Gabriel Moll Goñalons <47089710+TheBillyBox@users.noreply.github.com>
2021-10-05 18:58:15 +0200

    Alumnes option

--- controllers/myFirstController.js
+++ controllers/myFirstController.js
@@ -4,3 +4,26 @@ module.exports.helloWorld = (req, res) => {
         const responseStatus = 200;
         res.status(responseStatus).send(responseObj);
 }
+module.exports.alumnes = (req, res) => {
+        const responseObj = {
+                "Alumnes": [
+                  "Gerard",
+                  "Khadija",
+                  "Alex",
+                  "Roger",
+                  "Xavier",
+                  "Eric",
+                  "Miquel",
+                  "Alba",
+                  "Raul",
+                  "Marc",
+                  "Jordi",
+                  "Marc",
+                  "Pol",
+                  "Biel"
+                ],
+                "AlumnesCount": 14
+              }
+        const responseStatus = 200;
+        res.status(responseStatus).send(responseObj);
+}
--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const app = express();
 const myFirstController = require('./controllers/myFirstController')
 
 app.get('/', myFirstController.helloWorld)
+app.get('/alumnes', myFirstController.alumnes)
 
-app.listen(3000, () =>{
+app.listen(process.env.PORT || 3000, () =>{
     console.log('My Frist API running!')
 })
 
-//!  Si nodemon falla Set-ExecutionPolicy unrestricted
+//!  Si nodemon falla Set-ExecutionPolicy unrestricted en la PowerShell
